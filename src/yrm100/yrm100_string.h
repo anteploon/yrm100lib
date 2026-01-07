1c98bbb0285a661471622133eba9e1fe3d1aa577
Lauri Anteploon <lauri@anteploon.com>
2026-01-07 14:15:04 +0000

    Added comments

--- src/yrm100/yrm100_string.h
+++ src/yrm100/yrm100_string.h
@@ -24,49 +24,49 @@
 char *yrm100_get_tag_pc_string(rfid_tag_t *tag, char string_buf[YRM100_TAG_PC_STRING_LENGTH]);
 
 /**
- * @brief 
- * @param tag 
- * @param string_buf 
- * @return 
+ * @brief Formats the CRC part of the RFID tag as a hexadecimal string
+ * @param tag Pointer to RFID tag
+ * @param string_buf Pointer to string buffer
+ * @return Pointer to the string_buf
  */
 char *yrm100_get_tag_crc_string(rfid_tag_t *tag, char string_buf[YRM100_TAG_CRC_STRING_LENGTH]);
 
 /**
- * @brief 
- * @param tag 
- * @param string_buf 
- * @return 
+ * @brief Formats the EPC part of the RFID tag as a hexadecimal string
+ * @param tag Pointer to RFID tag
+ * @param string_buf Pointer to string buffer
+ * @return Pointer to the string_buf
  */
 char *yrm100_get_tag_epc_string(rfid_tag_t *tag, char string_buf[YRM100_TAG_EPC_STRING_LENGTH]);
 
 /**
- * @brief 
- * @param tag 
- * @param buf 
- * @return 
+ * @brief Formats the RSSI part of the RFID tag as a string with units
+ * @param tag Pointer to RFID tag
+ * @param buf Pointer to string buffer
+ * @return Pointer to the buf
  */
 char *yrm100_get_tag_rssi_string(rfid_tag_t *tag, char buf[YRM100_TAG_RSSI_STRING_LENGTH]);
 
 /**
- * @brief 
- * @param tag 
- * @param string_buf 
- * @return 
+ * @brief Formats the RFID tag details as a single descriptive string
+ * @param tag Pointer to RFID tag
+ * @param string_buf Pointer to string buffer
+ * @return Pointer to the string_buf
  */
 char *yrm100_get_tag_string(rfid_tag_t *tag, char string_buf[YRM100_TAG_STRING_LENGTH]);
 
 /**
- * @brief 
- * @param power 
- * @param string_buf 
- * @return 
+ * @brief Formats the transmit power value as a string, clamped to device limits
+ * @param power Transmit power in centi-dBm
+ * @param string_buf Pointer to string buffer
+ * @return Pointer to the string_buf
  */
 char *yrm100_convert_to_tx_power_string(unsigned int power, char string_buf[YRM100_PARAM_TX_POWER_STRING_LENGTH]);
 
 /**
- * @brief 
- * @param region_code 
- * @return 
+ * @brief Converts a region code to a human-readable string
+ * @param region_code Region code value
+ * @return Pointer to a region string
  */
 const char *yrm100_convert_to_region_string(unsigned int region_code);
 
